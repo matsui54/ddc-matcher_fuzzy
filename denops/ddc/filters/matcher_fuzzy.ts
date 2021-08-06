bed97c571b2a2cc9cf36dc45901e964a39205ba8
matsui54 <haru.matu9168@gmail.com>
2021-08-06 22:25:07 +0900

    First code

---
+++ denops/ddc/filters/matcher_fuzzy.ts
@@ -0,0 +1,55 @@
+import {
+  BaseFilter,
+  Candidate,
+  Context,
+  DdcOptions,
+  FilterOptions,
+  SourceOptions,
+} from "https://deno.land/x/ddc_vim@v0.0.10/types.ts#^";
+import { Denops } from "https://deno.land/x/ddc_vim@v0.0.10/deps.ts#^";
+
+function fuzzy_escape(str: string, camelcase: boolean): string {
+  let p = str.replace(/([a-zA-Z0-9])/g, "$1.*");
+  if (camelcase && str.match(/[A-Z]/)) {
+    p = p.replace(/([a-z])/, ((pat) => `[${pat.concat(pat.toUpperCase())}]`));
+  }
+  p = p.replace(/([a-zA-Z0-9_])\.\*/g, "$1[^$1]*");
+  return p;
+}
+
+export class Filter extends BaseFilter {
+  filter(
+    _denops: Denops,
+    _context: Context,
+    _options: DdcOptions,
+    sourceOptions: SourceOptions,
+    _filterOptions: FilterOptions,
+    filterParams: Record<string, unknown>,
+    completeStr: string,
+    candidates: Candidate[],
+  ): Promise<Candidate[]> {
+    if (sourceOptions.ignoreCase) {
+      completeStr = completeStr.toLowerCase();
+      const pattern = new RegExp(
+        fuzzy_escape(
+          completeStr.toLowerCase(),
+          filterParams.camelcase as boolean,
+        ),
+      );
+      return Promise.resolve(candidates.filter(
+        (candidate) =>
+          candidate.word.toLowerCase().startsWith(completeStr[0]) &&
+          candidate.word.toLowerCase().match(pattern),
+      ));
+    } else {
+      const pattern = new RegExp(
+        fuzzy_escape(completeStr, filterParams.camelcase as boolean),
+      );
+      return Promise.resolve(candidates.filter(
+        (candidate) =>
+          candidate.word.startsWith(completeStr[0]) &&
+          candidate.word.match(pattern),
+      ));
+    }
+  }
+}
