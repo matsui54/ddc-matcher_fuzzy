de769de0ec3c193e12efebf761446ade897f2541
matsui54 <haru.matu9168@gmail.com>
2021-08-10 11:36:55 +0900

    version up

--- denops/ddc/filters/deps.ts
+++ denops/ddc/filters/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/ddc_vim@v0.0.12/types.ts#^";
-export * from "https://deno.land/x/ddc_vim@v0.0.12/deps.ts#^";
+export * from "https://deno.land/x/ddc_vim@v0.0.13/types.ts#^";
+export * from "https://deno.land/x/ddc_vim@v0.0.13/deps.ts#^";
